87457c2a696fabb0db5dca543458df0396cfffba
Nexx1436 <adamori97@gmail.com>
2022-12-01 22:32:58 +0100

    Kezdetleges Webpage alkalmazás

---
+++ app/src/main/java/com/example/prm1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.prm1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://projektmunka2sze.000webhostapp.com/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    private class MywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if(webView.isFocused() && webView.canGoBack())
+        {
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
