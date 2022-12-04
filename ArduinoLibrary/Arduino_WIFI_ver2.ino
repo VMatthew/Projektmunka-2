d8e303fed24f403f76c200b2ae72ce6532cd5d70
Erdős Péter <112974335+eeP3ti@users.noreply.github.com>
2022-12-04 20:41:07 +0100

    + színelváltozás %
    + hibajavítások
    + Netrequest öntözés

--- ArduinoLibrary/Arduino_WIFI_ver2.ino
+++ ArduinoLibrary/Arduino_WIFI_ver2.ino
@@ -14,9 +14,13 @@
   //vízszint érzékelő érzékelő DATA-->>Analog 3 pin
   //Relé INPUT -->> Analog 5 pin
 
-#include <String.h>
-#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 #include <DHT.h>
+#define s0 8  
+#define s1 9
+#define s2 10
+#define s3 11
+#define out 12
 #define DHTPIN 2 //páratartalom és hőmérséklet érzékelő DATA-->>Digital 2 pin
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
@@ -25,6 +29,8 @@ int moisture;
 int luminosity;
 bool kifogyott;
 int waterlevel;
+int minviz = 400; //vízszint ami alatt öntözünk
+int Red=0, Blue=0, Green=0;  //RGB values 
 
 bool netrequest = false;
 String notfication ="";
@@ -39,8 +45,6 @@ const int szaraz = 810;
 const int nedves = 345;
 const int sotet = 300;
 const int fenyes = 700;
-
-const int minviz = 400; 
 const int relepin = A5; //Relé INPUT -->> Analog 5 pin
 
 // Wifi név és jelszó
@@ -53,11 +57,22 @@ const char* serverName = "https://projektmunka2sze.000webhostapp.com/";
 // PHP kóddal kompatibilis api key
 String apiKeyValue = "tPmAT5Ab3j7F9";
 
-String sensorName = "Sensors_4";
+String sensorName = "Sensors";
 String sensorLocation = "Home";
 
 void setup() {
   Serial.begin(115200);
+  pinMode(s0,OUTPUT);    //pin modes
+  pinMode(s1,OUTPUT);
+  pinMode(s2,OUTPUT);
+  pinMode(s3,OUTPUT);
+  pinMode(out,INPUT);
+  digitalWrite(s0,HIGH);
+  digitalWrite(s1,HIGH);
+  GetColors();
+  int ORed = Red;
+  int OGreen  = Green;
+  int OBlue = Blue;
   pinMode (relepin, OUTPUT);
   //pinMode (trigpin, OUTPUT);
   //pinMode (echopin, INPUT);
@@ -94,6 +109,13 @@ void loop() {
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature(false);
 
+  GetColors();
+  float Reddiff=ORed-Red;
+  float Greendiff=OGreen-Green;
+  float Bluediff=OBlue-Blue;
+  float x =  (abs(Reddiff) + abs(Greendiff) + abs(Bluediff))/765;
+  String ColourDistortion = String(x*100)+"%";
+
   sensorval = analogRead(A0); //Talajnedvesség érzékelő DATA-->>Analog 0 pin
   moisture = map(sensorval, nedves, szaraz, 100, 0);
   sensorval = analogRead(A2); //Fény érzékelő DATA-->>Analog 2 pin
@@ -107,6 +129,7 @@ void loop() {
   else {
     kifogyott = false;
   }
+
   //Notfication
   if(humidity<40 && notification.indexOf("Pára++ ;") == -1) { notification+="Pára++ ;"; }
   if(temperature<15 && notification.indexOf("Temp++ ;") == -1) { notification+="Temp++ ;"; }
@@ -125,6 +148,15 @@ void loop() {
     delay (1000);
     digitalWrite (relepin, HIGH);
   }
+  
+  //netrequest alapján öntözés
+  if (netrequest == true and kifogyott == false){
+    if(notification.indexOf("Öntözés történt ;") == -1 ) {
+      digitalWrite (relepin, LOW);
+      delay (1000);
+      digitalWrite (relepin, HIGH);
+    }
+  }
 
   //max 49 char lehet ha minden notification hozzáadodik, itt ellenőrizzük
   if(notification.length() >= 50 ) {
@@ -141,14 +173,15 @@ void loop() {
       
      // Domain név URL-el vagy IP-vel
      http.begin(client, serverName);
-      
+    
      // Specify content-type header
      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
       
      // HTTP POST request adat előkészítése
      String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
-                            + "&location=" + sensorLocation + "&humidity=" + Shumidity)
-                            + "&temperature=" + Stemperature) + "&moisture=" + Smoisture + "" + "&luminosity=" + Sluminosity + "&notification=" + notification;
+                            + "&location=" + sensorLocation + "&humidity=" + Shumidity
+                            + "&temperature=" + Stemperature) + "&moisture=" + Smoisture 
+                            + "&luminosity=" + Sluminosity + "&colour="+ ColourDistortion + "&notification=" + notification;
      Serial.print("httpRequestData: ");      
      Serial.println(httpRequestData);
      notification="";
@@ -171,5 +204,19 @@ void loop() {
      Serial.println("WiFi Disconnected");
    }
   }
-  delay(5000);  
+  delay(30000);  
+}
+
+void GetColors()  
+{    
+  digitalWrite(s2, LOW);                                          
+  digitalWrite(s3, LOW);                                           
+  Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       
+  delay(20);  
+  digitalWrite(s3, HIGH);                                       
+  Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  delay(20);  
+  digitalWrite(s2, HIGH);  
+  Green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  delay(20);  
 }
