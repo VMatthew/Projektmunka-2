de818744fb9c5c376a60074c61bd1dcd5839927f
Nexx1436 <adamori97@gmail.com>
2022-12-04 19:33:55 +0100

    Responsive alkalmazás kinézet

--- app/src/androidTest/java/com/example/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.prm1;
+package com.example.android;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.prm1", appContext.getPackageName());
+        assertEquals("com.example.android", appContext.getPackageName());
     }
 }
