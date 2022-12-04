4f94b0d4fff336e36dd3985dc446c4ce051960af
Erdős Péter <112974335+eeP3ti@users.noreply.github.com>
2022-12-04 16:01:34 +0100

    Kiegészítés
    + vizszint
    + öntözés 1.0
    + belső delay

--- ArduinoLibrary/Arduino_WIFI_ver2.ino
+++ ArduinoLibrary/Arduino_WIFI_ver2.ino
@@ -10,41 +10,50 @@
 #include <String.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
-#define DHTPIN 2
+#define DHTPIN 2 //páratartalom és hőmérséklet érzékelő DATA-->>Digital 2 pin
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 int sensorval;
 int moisture;
 int luminosity;
+bool kifogyott;
+int waterlevel;
 
+/*int trigpin = 8;
+int echopin = 9;
+int duration;
+int distance;*/
+
+//Mért értékek a map függvényekhez
 const int szaraz = 810;
 const int nedves = 345;
 const int sotet = 300;
 const int fenyes = 700;
 
-// Replace with your network credentials
+const int minviz = 400; 
+const int relepin = A5; //Relé INPUT -->> Analog 5 pin
+
+// Wifi név és jelszó
 const char* ssid     = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-// REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "https://projektmunka2sze.000webhostapp.com/php/post-to-db.php";
+// weblap domain
+const char* serverName = "https://projektmunka2sze.000webhostapp.com/";
 
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
-String apiKeyValue = "d7a03fee5546592a37e22ff8f45bbbe45da4632dfed9a774e085d0e8b5d3fa73";
+// PHP kóddal kompatibilis api key
+String apiKeyValue = "tPmAT5Ab3j7F9";
 
 String sensorName = "Sensors_4";
 String sensorLocation = "Home";
 
-const int szaraz = 810;
-const int nedves = 345;
-const int sotet = 300;
-const int fenyes = 700;
-
 void setup() {
   Serial.begin(115200);
-  
+  pinMode (relepin, OUTPUT);
+  //pinMode (trigpin, OUTPUT);
+  //pinMode (echopin, INPUT);
+  digitalWrite (relepin, HIGH);  
   WiFi.begin(ssid, password);
+
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) { 
     delay(500);
@@ -53,57 +62,94 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+  dht.begin();
+  delay(1000);
 
 }
-
+/*
+int tavolsag(){
+  int xd;
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+  duration = pulseIn (echopin, HIGH);
+  xd = duration * 0.034 / 2;
+  return xd;
+}
+*/
 void loop() {
-  //Check WiFi connection status
+  //Változók feltöltése
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature(false);
 
-  sensorval = analogRead(A0);
+  sensorval = analogRead(A0); //Talajnedvesség érzékelő DATA-->>Analog 0 pin
   moisture = map(sensorval, nedves, szaraz, 100, 0);
-  sensorval = analogRead(A2);
+  sensorval = analogRead(A2); //Fény érzékelő DATA-->>Analog 2 pin
   luminosity = map(sensorval, fenyes, sotet, 100, 0);
+  waterlevel = analogRead(A3); //vízszint érzékelő érzékelő DATA-->>Analog 3 pin
 
+  if (waterlevel < minviz){
+    kifogyott = true;
+  }
+  else {
+    kifogyott = false;
+  }
+
+  //Stringbe konvertálás
   String Shumidity = String(humidity);
   String Stemperature = String(temperature)+" C°";
   String Smoisture = String(moisture)+"%";
   String Sluminosity = String(luminosity)+"%";
 
-  if(WiFi.status()== WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
-    
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, serverName);
-    
-    // Specify content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Prepare your HTTP POST request data
-    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + "&humidity=" + Shumidity)
-                          + "&temperature=" + Stemperature) + "&moisture=" + Smoisture + "" + "&luminosity=" + Sluminosity;
-    Serial.print("httpRequestData: ");
-    Serial.println(httpRequestData);
+  //nagyon alap öntöző kód
+  if (moisture <85 and kifogyott == false){
+    digitalWrite (relepin, LOW);
+    delay (1000);
+    digitalWrite (relepin, HIGH);
+  }
 
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(httpRequestData);
-        
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+  //Bár a delay 5 secre van rakva ez a rész csak 30 secenként fusson le
+  double current=0;
+  if(millis() - current >= 30000) {
+    current = millis();
+
+    //WIFI kapcsolat ellenőrzése
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      
+      // Domain név URL-el vagy IP-vel
+      http.begin(client, serverName);
+      
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      // HTTP POST request adat előkészítése
+      String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                            + "&location=" + sensorLocation + "&humidity=" + Shumidity)
+                            + "&temperature=" + Stemperature) + "&moisture=" + Smoisture + "" + "&luminosity=" + Sluminosity;
+      Serial.print("httpRequestData: ");
+      Serial.println(httpRequestData);
+
+      // HTTP POST küldése
+      int httpResponseCode = http.POST(httpRequestData);
+          
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // tárhely felszabadítása
+      http.end();
     }
     else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
+      Serial.println("WiFi Disconnected");
     }
-    // Free resources
-    http.end();
   }
-  else {
-    Serial.println("WiFi Disconnected");
-  }
-  //Send an HTTP POST request every 30 seconds
-  delay(30000);  
+  delay(5000);  
 }
